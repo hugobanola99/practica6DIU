acb0120a5b90c5b143f93597debb1ce16e6c727d
hugobanola99 <hugo.banola101@alu.ulpgc.es>
2021-10-25 18:27:01 +0100

    Version provisional

---
+++ src/main/java/com/mycompany/practica6diu/Main.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.practica6diu;
+
+/**
+ *
+ * @author hugob
+ */
+public class Main {
+    public static void main(String[] args) {
+        Practica6 frame = new Practica6();
+        frame.setVisible(true);
+    }
+    
+}
